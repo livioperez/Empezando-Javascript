e3768db39eb6a87477b527936608be70727686af
livioperez <daniellperez@hotmail.com>
2022-03-10 12:22:37 -0300

    empezando el proyecto

---
+++ java.js
@@ -0,0 +1,8 @@
+/* let nombreIngresado = prompt("Ingrese su nombre") */
+ /* alert("Hola Dani") */
+
+
+ 
+  let entrada = prompt("nombre");
+  let salida = "¡hola"+ " "+ entrada + "!";
+  alert(salida);
